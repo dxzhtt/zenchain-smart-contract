bd41757911a570c5aba56dcb4a8688516d0f097f
Dxzhtt <wahyuramadhani371@gmail.com>
2025-05-21 22:24:10 +0700

    Add Mycontract.sol

--- Contracts/Mycontract.sol
+++ Contracts/Mycontract.sol
@@ -1,1 +1,15 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MyContract {
+    uint256 public counter;
+
+    function increment() public {
+        counter++;
+    }
+
+    function getCounter() public view returns (uint256) {
+        return counter;
+    }
+}
